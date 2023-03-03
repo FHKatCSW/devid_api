2a44573024477e172d65daf76e1031141df41a2c
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-03 16:50:46 +0100

    update

--- app/apis/Ldev_service_interface.py
+++ app/apis/Ldev_service_interface.py
@@ -3,9 +3,9 @@ from flask_restx import Namespace, Resource, fields
 api = Namespace("LDevID", description="IEEE 802.1 AR LDevID Service related operations")
 
 
-@api.route('/<name>', endpoint='create')
+@api.route('/key/<name>', endpoint='ldevid-key')
 @api.doc(params={'name': 'A name'})
-class LDevID(Resource):
+class LDevIDKey(Resource):
 
     @api.doc("create")
     def post(self, name):
@@ -14,33 +14,39 @@ class LDevID(Resource):
 
     @api.doc("delete")
     def delete(self, name):
-        """Delete an LDevID certificate"""
+        """Delete an LDevID key"""
         return {"status": "NotImplemented"}
 
     @api.doc("get")
+    @api.doc(params={'test': 'A test'})
     def get(self, name):
         """Export an LDevID certificate"""
         return {"status": "NotImplemented"}
 
-
-@api.route('/provision/<name>', endpoint='provision-ldevid')
+@api.route('/cert/<name>', endpoint='ldevid-cert')
 @api.doc(params={'name': 'A name'})
-class ProvisionLDevID(Resource):
+class LDevIDKey(Resource):
 
     @api.doc("provision")
-    def post(self, name):
-        """Provision an LDevID certificate"""
+    def provision(self, name):
+        """Create an LDevID key"""
         return {"status": "NotImplemented"}
-
-
-@api.route('/verify/<name>', endpoint='verify-ldevid')
-@api.doc(params={'name': 'A name'})
-class VerifyLDevID(Resource):
 
     @api.doc("post")
-    def post(self, name):
-        """Verify an LDevID certificate"""
+    def create(self, name):
+        """Create an LDevID key"""
         return {"status": "NotImplemented"}
+
+    @api.doc("delete")
+    def delete(self, name):
+        """Delete an LDevID key"""
+        return {"status": "NotImplemented"}
+
+    @api.doc("get")
+    def export(self, name):
+        """Export an LDevID certificate"""
+        return {"status": "NotImplemented"}
+
 
 
 
