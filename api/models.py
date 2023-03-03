63f04c1450e204c7192a4ac1f2291a991d5d29f2
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-03 11:01:54 +0100

    update

--- api/models.py
+++ api/models.py
@@ -9,28 +9,7 @@ import json
 
 class Datas():
 
-    id           = db.Column(db.Integer()   , primary_key=True)
-    data         = db.Column(db.String(256) , nullable=False)
-    date_created = db.Column(db.DateTime()  , default=datetime.utcnow)
-
-    def __repr__(self):
-        return str( self.id ) 
-
-    def update_data(self, new_data):
-        self.data = new_data
-
-    @classmethod
-    def get_by_id(cls, id):
-        return cls.query.filter_by(id=id).first()
-
-    def toDICT(self):
-
-        cls_dict         = {}
-        cls_dict['_id']  = self.id
-        cls_dict['data'] = self.data
-
-        return cls_dict
-
-    def toJSON(self):
+    def __init__(self):
+        print("Address of self = ",id(self))
 
         return self.toDICT()
