35101856795445f5507e9adf20f01af5c88030b5
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-03 11:31:22 +0100

    update

--- api/restx.py
+++ api/restx.py
@@ -4,6 +4,9 @@ from flask_restx import Api
 from endpoints import ns_ideen  # Importieren der API-Endpoints
 from endpoints import ns_kategorien  #
 
+app = Flask(__name__)  # ohne Starten der Hauptanwendung testen
+
+
 api = Api(  # Metadaten der API
     title="Ideenbriefkasten API",
     version="1.0",
@@ -15,7 +18,5 @@ api.add_namespace(ns_kategorien, path="/kategorien")  #
 bp = Blueprint('api', __name__, url_prefix='/api')  # Initialisieren der API unter der URL /api
 api.init_app(bp)  #
 
-if __name__ == '__main__':  # So lässt sich die API
-    app = Flask(__name__)  # ohne Starten der Hauptanwendung testen
 app.register_blueprint(bp)  #
-app.run(debug=True)  #
+
--- run.py
+++ run.py
@@ -4,7 +4,7 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from api.restx import api
+from api.restx import app
 
-if __name__ == '__main__':
-    api.run(debug=True, host="0.0.0.0")
+if __name__ == '__main__':  # So lässt sich die API
+    app.run(debug=True)  #
