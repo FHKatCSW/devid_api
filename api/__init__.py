d5bc65c3ae473d8040ff866ada7817bba3b1cf7c
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-03 11:06:16 +0100

    update

--- api/__init__.py
+++ api/__init__.py
@@ -16,16 +16,9 @@ app = Flask(__name__)
 # load Config 
 app.config.from_object('api.config.BaseConfig')
 
-# Connect Flask and SqlAlchemy
-db.init_app(app)
-
 # Connect Flask and FlaskRestX
 rest_api.init_app(app)
 
 # Enable CORS
 CORS(app)
 
-# Setup database
-@app.before_first_request
-def initialize_database():
-    db.create_all()
