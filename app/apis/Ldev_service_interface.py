7f967b8541f5172c6c86110248cfa3f0fd2ab452
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-06 08:50:34 +0100

    update

--- app/apis/Ldev_service_interface.py
+++ app/apis/Ldev_service_interface.py
@@ -9,8 +9,6 @@ class LDevIDKey(Resource):
     @api.doc("delete")
     def delete(self, keyIndex):
         """LDevID key delete (7-2-10): The DevID module performs cryptographic zeroization on LDevID key storage as part of the delete process, removing both private and public key material"""
-        # The DevID module performs cryptographic zeroization on LDevID key storage as part of the delete
-        # process, removing both private and public key material
         return {"status": "NotImplemented"}
 
 @api.route('/key/generate/<keyIndex>', endpoint='ldevid-key-generate')
@@ -19,9 +17,7 @@ class LDevIDKeyGenerate(Resource):
 
     @api.doc("create")
     def post(self, keyIndex):
-        """LDevID key generate (7.2.8)"""
-        # This operation allows the device administrator to generate an additional LDevID key within the DevID
-        # module. Newly generated keys are disabled and need to be explicitly enabled before use.
+        """LDevID key generate (7-2-8): This operation allows the device administrator to generate an additional LDevID key within the DevID module - Newly generated keys are disabled and need to be explicitly enabled before use"""
         return {"status": "NotImplemented"}
 
 @api.route('/key/insert/<keyIndex>', endpoint='ldevid-key-insert')
@@ -29,9 +25,7 @@ class LDevIDKeyGenerate(Resource):
 class LDevIDKeyInsert(Resource):
     @api.doc("post")
     def post(self, keyIndex):
-        """LDevID key insert (7.2.9)"""
-        # This operation allows the device administrator to insert an externally generated LDevID key into the DevID
-        # module.
+        """LDevID key insert (7.2.9): This operation allows the device administrator to insert an externally generated LDevID key into the DevID module"""
         return {"status": "NotImplemented"}
 
 @api.route('/cert/<certificateIndex>', endpoint='ldevid-cert')
@@ -45,17 +39,15 @@ class LDevIDCert(Resource):
 
     @api.doc("delete")
     def delete(self, certificateIndex):
-        """LDevID certificate delete (7.2.13)"""
-        # This operation implicitly deletes any certificate chain associated with the deleted certificate, it does not
-        # remove the associated DevID secret. This operation does not delete an IDevID certificate even if identified
-        # by the certificateIndex. The DevID module performs cryptographic zeroization on LDevID certificate material as part of the
-        # delete process.
+        """LDevID certificate delete (7.2.13): This operation implicitly deletes any certificate chain associated with the deleted certificate, it does not
+        remove the associated DevID secret - This operation does not delete an IDevID certificate even if identified
+        by the certificateIndex - The DevID module performs cryptographic zeroization on LDevID certificate material as part of the
+        delete process"""
         return {"status": "NotImplemented"}
 
     @api.doc("get")
     def get(self, certificateIndex):
-        """LDevID certificate export (NOT STANDARDIZED)"""
-        # Exports a certificate associated to a certificateIndex.
+        """LDevID certificate export (NOT STANDARDIZED): Exports a certificate associated to a certificateIndex"""
         return {"status": "NotImplemented"}
 
 @api.route('/chain/<certificateIndex>', endpoint='ldevid-chain')
@@ -64,21 +56,18 @@ class LDevIDChain(Resource):
 
     @api.doc("post")
     def post(self, certificateIndex):
-        """LDevID certificate chain insert (7.2.12)"""
-        # A certificate chain to be associated with a certificateIndex
+        """LDevID certificate chain insert (7-2-12): A certificate chain to be associated with a certificateIndex"""
         return {"status": "NotImplemented"}
 
     @api.doc("get")
     def get(self, certificateIndex):
-        """LDevID certificate chain export (NOT STANDARDIZED)"""
-        # Exports a certificate chain associated to a certificateIndex.
+        """LDevID certificate chain export (NOT STANDARDIZED): Exports a certificate chain associated to a certificateIndex"""
         return {"status": "NotImplemented"}
 
     @api.doc("delete")
     def delete(self, certificateIndex):
-        """LDevID certificate chain delete (7.2.14)"""
-        # The DevID module performs cryptographic zeroization on LDevID certificate chain material as part of
-        # the delete process.
+        """LDevID certificate chain delete (7-2-14): The DevID module performs cryptographic zeroization on LDevID certificate chain material as part of the delete process"""
+
         return {"status": "NotImplemented"}
 
 
