9e43895a246df8f7f8218b261740141321e27d00
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-04-04 19:31:37 +0200

    update

--- app/apis/highlevel_ldev_service_interface.py
+++ app/apis/highlevel_ldev_service_interface.py
@@ -36,8 +36,26 @@ class HighLvlLdevProvision(Resource):
     @api.doc("post")
     def post(self):
         """Only for demonstration purpose: Provision a new IDevID (key + cert) via Azure"""
-        return {"success": True,
-                "message": "NotImplemented"}
+        try:
+            ldevid = BootstrapDevId(pin="1234", slot=0)
+            ldevid.setup_ldev_id()
+            ldevid.validate_idev_certifificate(
+                ca_chain_url="https://campuspki.germanywestcentral.cloudapp.azure.com/ejbca/publicweb/webdist/certdist?cmd=cachain&caid=-1791256346&format=pem")
+            ldevid.create_key()
+            ldevid.generate_csr()
+            ldevid.request_cert(base_url='campuspki.germanywestcentral.cloudapp.azure.com',
+                                p12_file='/home/admin/certs/fhk_hmi_setup_v3.p12',
+                                p12_pass='foo123',
+                                certificate_profile_name='DeviceIdentity-Raspberry',
+                                end_entity_profile_name='KF-CS-EE-DeviceIdentity-Raspberry',
+                                certificate_authority_name='KF-CS-HMI-2023-CA')
+            ldevid.import_certificate()
+            ldevid.configure_azure()
+            return {"success": True,
+                    "message": "Bootstrap done"}
+        except Exception as err:
+            return {"success": False,
+                    "message": str(err)}
 
 @api.route('/actual', endpoint='highlvl-ldev-get')
 class HighLvlLdevProvision(Resource):
---
+++ app/core/adapters/id_manager.py
@@ -0,0 +1,37 @@
+import json
+import datetime
+import os
+
+
+class IDManager:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+        self.check_json_file()
+
+        with open(self.file_path, 'r') as file:
+            self.ids = json.load(file)
+
+    def check_json_file(self):
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, 'w') as file:
+                json.dump([], file)
+
+    def add_id(self, id_value):
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.ids.append({'id': id_value, 'timestamp': timestamp})
+
+        with open(self.file_path, 'w') as file:
+            json.dump(self.ids, file)
+
+    def get_latest_id(self):
+        if len(self.ids) == 0:
+            return None
+        else:
+            return max(self.ids, key=lambda x: x['timestamp'])['id']
+
+if __name__ == "__main__":
+    id = IDManager()
+    id.add_id(id_value="10542d997f0000001000000000000000")
+    most_recent = id.get_latest_id()
+    print(most_recent)
