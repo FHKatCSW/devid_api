e98258a87ca493ae1a5810b18f33ad1470546534
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-04-05 11:54:25 +0200

    update

--- app/apis/adapters/cert_handler.py
+++ app/apis/adapters/cert_handler.py
@@ -107,6 +107,8 @@ class CertHandler:
         # Extract the CN, O, and OU fields from the subject
         self.parsed_cert["cn"] = subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)[0].value
         self.logger.info("--general info done")
+        self.logger.info("{}".format(serial_num_attr[0].value))
+
 
         self.parsed_cert["o"] = org_name_attr[0].value if org_name_attr else None
         self.parsed_cert["ou"] = org_unit_name_attr[0].value if org_unit_name_attr else None
