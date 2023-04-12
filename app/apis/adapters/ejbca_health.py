24bb88970288b8121db0139f0f10074f2ef7fca3
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-04-12 09:31:48 +0200

    update

---
+++ app/apis/adapters/ejbca_health.py
@@ -0,0 +1,52 @@
+import json
+import requests
+from requests_pkcs12 import Pkcs12Adapter
+import OpenSSL.crypto
+from app.apis.adapters import logger
+import base64
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+
+
+class EjbcaHealth:
+    def __init__(self, base_url, p12_file, p12_pass):
+        self.logger = logger.get_logger("CertRequest")
+
+        self.base_url = base_url
+        self.p12_file = p12_file
+        self.p12_pass = p12_pass
+
+
+
+    def health_status(self):
+        # Create JSON payload
+        try:
+            url = f'https://{self.base_url}/ejbca/ejbca-rest-api/v1/certificate/status'
+
+            # Send request
+            session = requests.Session()
+            session.mount(url, Pkcs12Adapter(max_retries=3, pkcs12_filename=self.p12_file, pkcs12_password=self.p12_pass))
+            response = session.post(
+                url=url,
+                headers={'Content-Type': 'application/json', 'Accept': 'application/json'},
+                verify=False
+            )
+            response.raise_for_status()  # raise an HTTPError if status code is >= 400
+            response = json.loads(response.text)
+            self.logger.info("--Response: {}".format(response))
+        except requests.exceptions.HTTPError as err:
+            self.logger.error("HTTP error occurred:", err)
+        except requests.exceptions.RequestException as err:
+            self.logger.error("An error occurred:", err)
+        except (json.JSONDecodeError, OSError, KeyError) as e:
+            self.logger.error(f"Error requesting certificate: {str(e)}")
+
+
+if __name__ == "__main__":
+    health = EjbcaHealth(
+        base_url='campuspki.germanywestcentral.cloudapp.azure.com',
+        p12_file='/home/admin/fhk_hmi_setup_v3.p12',
+        p12_pass='foo123',
+    )
+    health.health_status()
